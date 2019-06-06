acd23f494fd03534c0df0d9e27565b88aa6119df
Jess Rezac <jess.rezac@gmail.com>
2019-06-06 21:02:03 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -21,6 +21,10 @@ class Triangle
     side_1 == side_2 || side_2 == side_3 || side_1 == side_3
   end
 
+  def scalene?
+    side_1 != side_2 || side_2 != side_3 || side_1 != side_3
+  end
+
   def kind
     unless valid?
       raise TriangleError
