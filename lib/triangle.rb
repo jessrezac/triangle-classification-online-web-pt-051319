477be238da6d75927b8b5592cebb8ab45298396a
Jess Rezac <jess.rezac@gmail.com>
2019-06-06 21:01:26 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,12 +10,28 @@ class Triangle
   end
 
   def valid?
-    side_1 + side_2 > side_3 && side_2 + side_3 > side_1 && side_1 + side_3 > side_2 && side_1, side_2, side_3 > 0
+    side_1 + side_2 > side_3 && side_2 + side_3 > side_1 && side_1 + side_3 > side_2
+  end
+
+  def equilateral?
+    side_1 == side_2 && side_2 == side_3
+  end
+
+  def isosceles?
+    side_1 == side_2 || side_2 == side_3 || side_1 == side_3
   end
 
   def kind
     unless valid?
       raise TriangleError
+    end
+
+    if equilateral?
+      :equilateral
+    elsif isosceles?
+      :isosceles
+    elsif scalene?
+      :scalene
     end
   end
 
