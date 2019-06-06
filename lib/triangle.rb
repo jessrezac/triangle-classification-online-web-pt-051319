a3ad091add0ec001d14b9bde150e4ad9471d37b1
Jess Rezac <jess.rezac@gmail.com>
2019-06-06 20:43:34 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,12 +1,17 @@
 class Triangle
+  attr_reader :side_1, :side_2, :side_3
 
-
-  def initialize(side1, side2, side3)
-
+  def initialize(side_1, side_2, side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
   end
 
   def kind
-
+    case self
+    when self.side_1 == self.side_2 && self.side_2 == self.side_3
+      return :equilateral
+    end
   end
 
   class TriangleError < StandardError
